3d9b709f093707603e0b5b5109baae29f91f55d4
MohamedShehab <midoshehab335@gmail.com>
2021-04-01 16:02:08 +0200

    Model Entites #3

---
+++ back-end/src/main/java/com/spring/boot/restaurant/model/Order.java
@@ -0,0 +1,10 @@
+package com.spring.boot.restaurant.model;
+
+public class Order {
+
+    private Long id;
+    private String nameOrder;
+    private String img;
+    private int price;
+    private String description;
+}
