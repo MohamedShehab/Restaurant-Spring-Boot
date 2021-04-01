61c39cde29359602f3b86b614c0fd62beeb41890
MohamedShehab <midoshehab335@gmail.com>
2021-04-01 16:54:48 +0200

    configuartion mysql #6

--- back-end/src/main/java/com/spring/boot/restaurant/model/BaseEntity.java
+++ back-end/src/main/java/com/spring/boot/restaurant/model/BaseEntity.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
 }
--- back-end/src/main/java/com/spring/boot/restaurant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/boot/restaurant/model/CategoryOrder.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class CategoryOrder extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 }
